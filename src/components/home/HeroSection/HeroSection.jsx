96e23f21d12b5a6aa7e00ccbac4772d070faa92c
kalidashodekare14 <kalidashodekare0@gmail.com>
2026-04-07 13:19:28 +0600

    Home page HeroSection design updated

--- src/components/home/HeroSection/HeroSection.jsx
+++ src/components/home/HeroSection/HeroSection.jsx
@@ -8,9 +8,22 @@ const HeroSection = () => {
                 {/* Left: Info */}
                 <div className="lg:w-200 space-y-5">
                     <h1 className="text-6xl font-bold">Grow Your Business with Growify</h1>
-                    <p>We create stunning websites, optimize your SEO for maximum visibility, and run effective digital campaigns to help your business grow and reach more customers online.</p>
-                    <div>
-                        <Button name={"Discover More"} isIcon={true} />
+                    <p className="text-[#111111bb]">We create stunning websites, optimize your SEO for maximum visibility, and run effective digital campaigns to help your business grow and reach more customers online.</p>
+                    <div className="flex lg:flex-row flex-col items-center gap-10">
+                        <div>
+                            <Button name={"Discover More"} isIcon={true} />
+                        </div>
+                        <div className="flex items-center gap-3">
+                            <div className="flex items-center">
+                                <img className=" w-12 h-12 border border-vivid rounded-full" src="/banner/img1.jpg" alt="" />
+                                <img className="-ml-4 w-12 h-12 border border-vivid rounded-full" src="/banner/img2.jpg" alt="" />
+                                <img className="-ml-4 w-12 h-12 border border-vivid rounded-full" src="/banner/img3.jpg" alt="" />
+                                <div className="-ml-4 w-12 h-12 bg-vivid text-white rounded-full flex justify-center items-center">
+                                    <p>50k</p>
+                                </div>
+                            </div>
+                            <p className="text-[#111111bb]">Our 50k + Satisfaction Clients</p>
+                        </div>
                     </div>
                 </div>
                 {/* Right: Image */}
