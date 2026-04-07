7823f9f1375f8c16a0a6e495805cc72587037fc2
kalidashodekare14 <kalidashodekare0@gmail.com>
2026-04-07 16:42:33 +0600

    Home page Pertners section design updatedd

--- src/components/home/Partners.jsx
+++ src/components/home/Partners.jsx
@@ -3,6 +3,7 @@ import SectionHeadLine from "../shared/SectionHeadLine";
 const Partners = () => {
     return (
         <div className="2xl:w-350 xl:w-310 lg:w-260 w-full m-auto px-5 lg:px-0">
+            {/* Header line */}
             <SectionHeadLine title={"Parners"} description={"Partnering with brands that trust us."} />
             {/* Partners Logo */}
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 mt-10">
