9377e74813bf8372304914cd680816b2540d2fed
kalidashodekare14 <kalidashodekare0@gmail.com>
2026-04-08 15:05:28 +0600

    Contact page ContactMap section design update

--- src/components/contact/ContactMap.jsx
+++ src/components/contact/ContactMap.jsx
@@ -1,18 +1,12 @@
+import SectionHeadLine from '.././shared/SectionHeadLine'
 const ContactMap = () => {
     return (
         <section className="py-16">
             <div className="2xl:w-350 xl:w-310 lg:w-260 w-full m-auto py-10 px-5">
-
-                <h2 className="text-3xl font-bold mb-4">
-                    Our Office Location
-                </h2>
-
-                <p className="text-gray-600 mb-8">
-                    Visit us or find us on the map below.
-                </p>
+                <SectionHeadLine title={"Our Office Location"} description={"Visit us or find us on the map below."} />
 
                 {/* Google Map */}
-                <div className="w-full h-100 rounded-2xl overflow-hidden shadow">
+                <div className="w-full h-100 rounded-2xl overflow-hidden shadow mt-10">
                     <iframe
                         src="https://www.google.com/maps?q=Dhaka,Bangladesh&output=embed"
                         className="w-full h-full border-0"
